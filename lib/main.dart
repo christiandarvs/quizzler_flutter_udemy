40e01ff993841838e30537c1f0929ab7372ae4b2
christiandarvs <christiandarvin4@gmail.com>
2023-02-13 23:00:38 +0800

    add googlefonts dependecy and quiz page contents

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,16 +8,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      home: QuizPage(),
     );
   }
 }
@@ -27,8 +21,57 @@ class QuizPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey.shade900,
       body: SafeArea(
-        child: Container(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(
+                alignment: Alignment.center,
+                // color: Colors.blue,
+                margin: const EdgeInsets.all(20),
+                child: Text(
+                  'Which NBA team won the NBA Championship six times during the 1990s?',
+                  style: GoogleFonts.poppins(color: Colors.white, fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: (() {}),
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                child: const Text('True'),
+              ),
+            )),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: (() {}),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red.shade400),
+                  child: const Text('False'),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
